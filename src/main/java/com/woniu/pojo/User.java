3c9b0b03967814ca4e7d8ed1d7883e9a494d480f
jia <jia@163.com>
2019-12-09 19:38:26 +0800

    creat usersetget

---
+++ src/main/java/com/woniu/pojo/User.java
@@ -0,0 +1,19 @@
+package com.woniu.pojo;
+
+public class User {
+	private Integer uid;
+	private String uname;
+	public Integer getUid() {
+		return uid;
+	}
+	public void setUid(Integer uid) {
+		this.uid = uid;
+	}
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	
+}
